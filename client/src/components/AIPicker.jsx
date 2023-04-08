cff13036ad86285e5902723b58c493d0d1c29675
fenroe <scrambles9494@gmail.com>
2023-04-09 01:50:34 +0300

    first draft

---
+++ client/src/components/AIPicker.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import CustomButton from './CustomButton'
+
+const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+  return (
+    <div className="aipicker-container">
+      <textarea
+      placeholder="Ask AI..."
+      rows={5} values={prompt}
+      onChange={(e) => setPrompt(e.target.value)}
+      className="aipciker-textarea" />
+      <div className="flex flex-wrap gap-3">
+        {generatingImg ? (
+          <CustomButton type="outline" title="Asking AI..." customStyles="text-xs" />
+        ) : (
+          <>
+            <CustomButton type="outline" title="AI Logo" handleClick={() => handleSubmit('logo')} customStyles="text-xs" />
+            <CustomButton type="outline" title="AI Full" handleClick={() => handleSubmit('full')} customStyles="text-xs" />
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default AIPicker
